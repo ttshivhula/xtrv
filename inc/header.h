13da5d558c8c07580d4df7acda45bb39aabca9cd
ttshivhula <pboyfunclub@gmail.com>
2018-09-22 17:40:13 +0200

    .

--- inc/header.h
+++ inc/header.h
@@ -1,15 +1,15 @@
 #ifndef HEADER_H
 # define HEADER_H
 
-#include <stdio.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <dirent.h>
-#include <sys/mman.h>
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <string.h>
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <fcntl.h>
+# include <dirent.h>
+# include <sys/mman.h>
 
 # define MAGIC 0x42a42a42a
 
